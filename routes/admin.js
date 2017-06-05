8ac3a25569e4d7735482b1e43c255e737a7993bd
MagicConch <350043743@qq.com>
2017-06-05 16:44:13 +0800

    ajax finished

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,11 @@ const Essay = require('../models/essay');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  Essay.find({}).exec(function(err, lists) {
+  Essay.find({}).sort({update_time: -1}).exec(function(err, lists) {
     if(err) throw err;
     lists.forEach(function(list) {
       list.content = list.content.slice(0,40);
-      list.editURL = `<a href='/api/edit/${list._id}'>编辑</a>`;
+      list.editURL = `<div class="ui primary button in edit" href='${list._id}'>编辑</div>`;
       list.delURL = `<div class="ui secondary button in del" href='/api/del/${list._id}'>删除</div>`;
     });
     res.render('admin', {
@@ -15,6 +15,16 @@ router.get('/', function(req, res) {
     });
   }); 
 });
+
+router.get('/article/:id',(req, res) => {
+  let id = req.params.id
+  Essay.find({ "_id" : id}).exec((err, essay) => {
+    if(err) throw err
+    res.json({
+      essay: essay[0]
+    })
+  })
+})
 
 router.get('/edit/:id', function(req, res) {
   
@@ -34,15 +44,18 @@ router.get('/edit/:id', function(req, res) {
 router.post('/edit/:id', function(req, res) {
   let id = req.params.id;
   let essay = {};
+  console.log(req.body)
   essay.title = req.body.title;
   essay.content = req.body.content;
   essay.update_time = new Date();
   console.log()
   Essay.findByIdAndUpdate(id, essay, function(err) {
     if(err) throw err;
-    res.redirect('/api');
-  });
-});
+    res.json({
+      success: 1
+    })
+  })
+})
 
 
 router.get('/add', function(req, res) {
@@ -51,17 +64,17 @@ router.get('/add', function(req, res) {
 
 router.post('/add', function(req, res) {
   var title = req.body.title;
-  var tags = req.body.tags;
   var content = req.body.content;
   var essay = new Essay();
   essay.title = title;
   essay.content = content;
   essay.create_time = new Date();
   essay.update_time = new Date();
-  essay.save(function(err) {
+  essay.save(function(err,product) {
     if(err) throw err;
-    console.log('meow');
-    res.redirect('/api'); 
+    res.json({
+      id: product._id
+    })
   });
   
 });
